69c04ec0aef66a4f418e04f5ae055face9c826dd
relu <relu.scarlat@gmail.com>
2020-02-16 17:21:31 +0200

    "Second commit"

---
+++ src/main/resources/static/precache-manifest.c5a2673df77e6e177cfd57c91f66a29d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01991caa6dbb741c48d066c4afdfa22c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff8ff25757a2313f1d8a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "761dab58b99ce6f0f4c9",
+    "url": "/static/css/main.86ded62d.chunk.css"
+  },
+  {
+    "revision": "ff8ff25757a2313f1d8a",
+    "url": "/static/js/2.7a732254.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.7a732254.chunk.js.LICENSE"
+  },
+  {
+    "revision": "761dab58b99ce6f0f4c9",
+    "url": "/static/js/main.21aef9dd.chunk.js"
+  },
+  {
+    "revision": "f5c3cf8bda414027413d",
+    "url": "/static/js/runtime-main.a16c7b0b.js"
+  }
+]);
