46425797bea5b1130ead57e4695eab88fc4c3b15
relu <relu.scarlat@gmail.com>
2020-02-18 03:56:26 +0200

    aaa

---
+++ target/classes/static/precache-manifest.fae5b8635200db87931669b40bf1103f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc941dd7e8b760b916045a58c178d72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff8ff25757a2313f1d8a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "34f8c0855c8b739e24ad",
+    "url": "/static/css/main.86ded62d.chunk.css"
+  },
+  {
+    "revision": "ff8ff25757a2313f1d8a",
+    "url": "/static/js/2.7a732254.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.7a732254.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34f8c0855c8b739e24ad",
+    "url": "/static/js/main.12890393.chunk.js"
+  },
+  {
+    "revision": "f5c3cf8bda414027413d",
+    "url": "/static/js/runtime-main.a16c7b0b.js"
+  }
+]);
